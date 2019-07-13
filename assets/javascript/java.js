9e4156cb53b89654769b8dad53931ff839219fae
unknown <tcutlip08@gmail.com>
2019-07-13 17:40:52 -0400

    displaying video on screen

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,1 +1,24 @@
-console.log("yo")
+$(document).ready(function () {
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true},
+            function (stream) {
+                var video = document.querySelector('video');
+                video.srcObject = stream;
+                video.onloadedmetadata = function (e) {
+                    video.play();
+                };
+            },
+            function (err) {
+                console.log("The following error occurred: " + err.name);
+            }
+        );
+    } else {
+        console.log("getUserMedia not supported");
+    }
+});
+
+console.log("hey");
