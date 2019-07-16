fc298e8e866c918f3f67a0bb71b84df5b3bb8ea8
Aaron Lanier <alanier.william@gmail.com>
2019-07-16 17:37:24 -0400

    used html and javascript to get access to camera and display it onscreen

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,1 +1,13 @@
 console.log("yo")
+
+var video = document.querySelector("#video-element");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
+}
