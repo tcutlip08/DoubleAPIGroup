016542d730230ce30ac514bc296113c66d61a506
unknown <tcutlip08@gmail.com>
2019-07-19 21:51:57 -0400

    nothing accomplished just trying to pull

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -40,71 +40,109 @@ $("#takePhoto").on("click", function () {
     var context = canvas.getContext('2d');
     var video = document.getElementById('liveVideo');
 
-    // console.log(canvas);
-    // console.log(context);
-    // console.log(video);
+    //     // console.log(canvas);
+    //     // console.log(context);
+    //     // console.log(video);
 
     context.drawImage(video, 0, 0, 400, 300);
 
-    // var request = new XMLHttpRequest();
+    var url = "https://api-us.faceplusplus.com/facepp/v3/detect"
+    var api_Key = "api_key=SGDsWC-LfRIlK-6AapwjGbUDWcHOR1gF"
+    var api_Secret = "api_secret=FLbaJQnABWlZEuXnxh16n-pgalV760vm"
+    // var photoToSearch = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    var photo = "https://mail.google.com/mail/u/0?ui=2&ik=469f8e69d7&attid=0.1&permmsgid=msg-a:r4724190948597656535&th=16bee75430922fb7&view=att&disp=safe&realattid=f_jy2drxwz0";
+    var returnAttributes = "return_attributes=emotion"
+    var queryURL = url + "?" + api_Key + "&" + api_Secret + "&image_url=" + photo + "&return_facekey";
 
-    // request.open('POST', 'https://api.kairos.com/enroll');
+    // https://scstylecaster.files.wordpress.com/2016/12/model-curly-hair-nose-ring.jpg
 
-    // request.setRequestHeader('Content-Type', 'application/json');
-    // request.setRequestHeader('app_id', '4985f625');
-    // request.setRequestHeader('app_key', 'aa9e5d2ec3b00306b2d9588c3a25d68e');
+    console.log(queryURL);
 
-    // request.onreadystatechange = function () {
-    //     if (this.readyState === 4) {
-    //         console.log('Status:', this.status);
-    //         console.log('Headers:', this.getAllResponseHeaders());
-    //         console.log('Body:', this.responseText);
-    //     }
-    // };
+    $.ajax({
+        url: queryURL,
+        method: "POST"
+    })
+        .then(function (response) {
+            console.log(response);
+            var results = response.data;
+            console.log(results);
 
-    // var body = {
-    //     'image': 'https://mail.google.com/mail/u/0?ui=2&ik=469f8e69d7&attid=0.1&permmsgid=msg-a:r4724190948597656535&th=16bee75430922fb7&view=att&disp=safe&realattid=f_jy2drxwz0',
-    //     'subject_id': 'Matt',
-    //     'gallery_name': 'MyGallery'
-    // };
+            // for (var i = 0; i < results.length; i++) {
 
-    // request.send(JSON.stringify(body));
+            //     var gifDiv = $("<div>");
+            //     gifDiv.addClass("card w-75 text-center");
 
-    //         for (var i = 0; i < results.length; i++) {
+            //     var rating = results[i].rating;
 
-    //             var gifDiv = $("<div>");
-    //             gifDiv.addClass("card w-75 text-center");
+            //     var p = $("<h5>").html("Rating: " + rating);
 
-    //             var rating = results[i].rating;
+            //     var gifImage = $("<img>");
+            //     gifImage.addClass("gif");
+            //     gifImage.attr("src", results[i].images.fixed_width_still.url);
+            //     gifImage.attr("data-still", results[i].images.fixed_width_still.url);
+            //     gifImage.attr("data-animate", results[i].images.fixed_width.url);
+            //     gifImage.attr("data-state", "still");
+            //     gifImage.attr("style", "padding: 10px");
 
-    //             var p = $("<h5>").html("Rating: " + rating);
+            //     var imageTitle = $("<h4>");
+            //     imageTitle.addClass("gifTitle");
+            //     var title = (results[i].title.split(" GIF"));
+            //     imageTitle.html(title[0].toUpperCase());
 
-    //             var gifImage = $("<img>");
-    //             gifImage.addClass("gif");
-    //             gifImage.attr("src", results[i].images.fixed_width_still.url);
-    //             gifImage.attr("data-still", results[i].images.fixed_width_still.url);
-    //             gifImage.attr("data-animate", results[i].images.fixed_width.url);
-    //             gifImage.attr("data-state", "still");
-    //             gifImage.attr("style", "padding: 10px");
+            //     var imageDetail = $("<div>");
+            //     imageDetail.addClass("imageDetail");
+            //     imageDetail.append(imageTitle);
 
-    //             var imageTitle = $("<h4>");
-    //             imageTitle.addClass("gifTitle");
-    //             var title = (results[i].title.split(" GIF"));
-    //             imageTitle.html(title[0].toUpperCase());
+            //     var favButton = $("<button>");
+            //     favButton.addClass("btn btn-primary").attr("id", "favBtn").text("✩Favorites");
 
-    //             var imageDetail = $("<div>");
-    //             imageDetail.addClass("imageDetail");
-    //             imageDetail.append(imageTitle);
+            //     gifDiv.prepend(favButton);
+            //     gifDiv.prepend(p);
+            //     gifDiv.prepend(imageDetail);
+            //     gifDiv.prepend(gifImage);
 
-    //             var favButton = $("<button>");
-    //             favButton.addClass("btn btn-primary").attr("id", "favBtn").text("✩Favorites");
-
-    //             gifDiv.prepend(favButton);
-    //             gifDiv.prepend(p);
-    //             gifDiv.prepend(imageDetail);
-    //             gifDiv.prepend(gifImage);
-
-    //             $("#displayGifs").prepend("<br>")
-    //             $("#displayGifs").prepend(gifDiv);
-    //     }
+            //     $("#displayGifs").prepend("<br>")
+            //     $("#displayGifs").prepend(gifDiv);
+            // }
+        });
 });
+
+const config = {
+    apiKey: "AIzaSyCDU6rYUeS3vBdWWsfuZGJQu07AHLGU7kU",
+    authDomain: "doubleapigroup-5d285.firebaseapp.com",
+    databaseURL: "https://doubleapigroup-5d285.firebaseio.com",
+    projectId: "doubleapigroup-5d285",
+    storageBucket: "doubleapigroup-5d285.appspot.com",
+    messagingSenderId: "518885986314",
+    appId: "1:518885986314:web:7767e302d24f7613"
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+const storageService = firebase.storage();
+const storageRef = storageService.ref();
+
+document.querySelector('.file-select').addEventListener('change', handleFileUploadChange);
+document.querySelector('.file-submit').addEventListener('click', handleFileUploadSubmit);
+
+let selectedFile;
+
+function handleFileUploadChange(e) {
+    selectedFile = e.target.files[0];
+}
+
+function handleFileUploadSubmit(e) {
+    const uploadTask = storageRef.child(`images/${selectedFile.name}`).put(selectedFile);
+    //create a child directory called images, and place the file inside this directory
+
+    uploadTask.on('state_changed', (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+    }, (error) => {
+        // Handle unsuccessful uploads
+        console.log(error);
+    }, () => {
+        // Do something once upload is complete
+        console.log('success');
+    });
+}
