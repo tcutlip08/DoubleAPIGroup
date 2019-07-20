3a52416ad7a9b5f618bc071a7dd484cc464643cf
Shahriar Elias <shahriar998@gmail.com>
2019-07-20 12:40:22 -0400

    my changes which i don't want to be added

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,14 +1,21 @@
-       function detectBrowser() {
-            var useragent = navigator.userAgent;
-            var mapdiv = document.getElementById("map");
-            if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
-                mapdiv.style.width = '100%';
-                mapdiv.style.height = '100%';
-            } else {
-                mapdiv.style.width = '600px';
-                mapdiv.style.height = '800px';
-            }
+      
+     $(document).on("click","#sadness-button",function(){
+
+        console.log()
+     }) 
+      
+      function detectBrowser() {
+        var useragent = navigator.userAgent;
+        var mapdiv = document.getElementById("map");
+        if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1) {
+            mapdiv.style.width = '100%';
+            mapdiv.style.height = '100%';
+        } else {
+            mapdiv.style.width = '600px';
+            mapdiv.style.height = '800px';
         }
+    }
+      
         var myLatLng;
         var latit;
         var longit;
@@ -118,4 +125,6 @@
                 alert("Geolocation is not supported by this browser.");
             }
         }
+      
+       
     
